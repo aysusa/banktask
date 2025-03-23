568da9a9b499741b9fae2e9ff26c31d927120de4
aysusa <aysusa-bb208@code.edu.az>
2025-03-23 21:22:24 +0400

    Add project files.

---
+++ banktask/Bank.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace banktask
+{
+    class Bank
+    {
+        public BankAccount[] bankAccounts;
+        public Bank()
+        {
+            bankAccounts = new BankAccount[0];
+        }
+
+        public void Add(BankAccount bankaccount)
+        {
+            Array.Resize(ref bankAccounts, bankAccounts.Length + 1);
+            bankAccounts[bankAccounts.Length - 1] = bankaccount;
+
+        }
+        public BankAccount GetBankAccountByOwner(string OwnerName)
+        {
+            BankAccount bankAccount = null;
+            for (int i = 0; i < bankAccounts.Length; i++)
+            {
+             if (bankAccounts[i].OwnerName == OwnerName)
+                {
+                    bankAccount = bankAccounts[i];
+                    break;
+                }
+             else
+                {
+                    Console.WriteLine("bele bir account yoxdu");
+                }
+            }
+            return bankAccount;
+
+        }
+        public void GetBankAccountsCount()
+        {
+            Console.WriteLine(bankAccounts.Length);
+        }
+        public void DeleteBankAccount(string OwnerName)
+        {
+            BankAccount bankAccount = null;
+            for(int i = 0; i < bankAccounts.Length; i++)
+            {
+                if (bankAccounts[i].OwnerName == OwnerName)
+                {
+                    bankAccounts[i] = bankAccount;
+                    Array.Resize(ref bankAccounts, bankAccounts.Length - 1);
+                    bankAccounts[bankAccounts.Length - 1] = bankAccount;
+                }
+            }
+        }
+    }
+}
---
+++ banktask/BankAccount.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace banktask
+{
+    class BankAccount
+    {
+		private decimal _balance;
+
+		public decimal balance
+		{
+			get { return _balance; }
+			
+		}
+		private int _accountnumber;
+
+		public int AccountNumber
+		{
+			get { return _accountnumber; }
+		
+		}
+        public string OwnerName { get; }
+        public BankAccount(int Accountnumber,string ownername,decimal Balance)
+        {
+			_accountnumber = Accountnumber;
+			OwnerName = ownername;
+			if(Balance>=0)
+			_balance = Balance;
+			
+        }
+		public void Deposit (decimal depositamount)
+		{
+			if(depositamount > 0)
+			{
+              _balance += depositamount;
+                Console.WriteLine($"{depositamount} azn ugurla kocuruldu, cari balance{_balance}");
+            }
+			else
+			{
+                Console.WriteLine("deposit meblegi 0dan kicik ola bilmez");
+			}
+				
+		}
+		public void WithDraw (decimal withdrawamount)
+		{
+			if (withdrawamount < _balance && _balance>0 )
+			{
+				_balance -= withdrawamount;
+                Console.WriteLine($"{ withdrawamount} azn balansdan cixarildi,cari balance{ _balance} ");
+			}
+			else if(withdrawamount<0)
+				{
+                Console.WriteLine("menfi ola bilmez");
+			}
+			else
+			{
+				Console.WriteLine("balansda kifayet qeder mebleg yoxdur");
+			}
+		}
+		public void DisplayAccountInfo()
+		{
+            Console.WriteLine($"Owner Name: {OwnerName},Balance: {balance},Account number:{AccountNumber}");
+		}
+		public void TransferFunds(BankAccount recipient,decimal amount)
+		{
+			recipient.Deposit(amount);
+            Console.WriteLine($"{recipient} shexsine {amount} azn qeder mebleg kocuruldu.");
+		}
+    }
+}
